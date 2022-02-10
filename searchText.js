e7cd4547c9c1b23218f7b0f1082bdf7f9bc808d2
Shihab <73121069+maker-shihab@users.noreply.github.com>
2022-02-10 23:50:34 +0600

    Code Update

--- searchText.js
+++ searchText.js
@@ -1,1 +1,17 @@
-console.log('Hello Search Result');
+const stores = [
+  {name: 'iPhone 14', price: 60000},
+  {name: 'qPhone 01', price: 2000},
+  {name: 'Nokia Phone', price: 32000},
+  {name: 'Lenovo', price: 45000},
+  {name: 'Redmi phone', price: 14000},
+  {name: 'hp Leptop', price: 58000},
+  {name: 'asus Leptop', price: 78000}
+];
+function findStore(stores) {
+  for (const store of stores){
+    console.log(store);
+  }
+}
+findStore(stores);
+// Hello bangladesh
+console.log('Hello Bangladesh');
